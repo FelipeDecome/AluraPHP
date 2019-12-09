be1dc5e7758db8e4e438dde4f1fd8b38027ba376
FelipeDecome <felipe.decome@hotmail.com>
2019-12-09 20:31:44 -0300

    Update Extrair Classe

--- Refatoração/extrair_classe/index.php
+++ Refatoração/extrair_classe/index.php
@@ -2,9 +2,14 @@
 
 namespace Alura\ExtrairClasse;
 
+require 'Contato.php';
 require 'Usuario.php';
 
-$usuario = new Usuario("Giovanni", "Tempobono", "Rua Vergueiro 3185", "04101-300", "5571-2751", "11");
+$contato = new Contato("Rua Vergueiro 3185", "04101-300", "5571-2751", "Fixo", "11");
+$usuario = new Usuario("Giovanni", "Tempobono", $contato);
 
 echo $usuario->getNome();
+echo "<br/>";
+echo $usuario->getEnderecoECep();
+echo "<br/>";
 echo $usuario->getTelefoneDdd();
