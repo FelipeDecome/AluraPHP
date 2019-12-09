1a7df4b1dac089feb5637725544784dbe6e4258f
FelipeDecome <felipe.decome@hotmail.com>
2019-12-09 18:57:19 -0300

    Update Extrair Váriavel

--- Refatoração/extrair_variavel/index.php
+++ Refatoração/extrair_variavel/index.php
@@ -1,8 +1,16 @@
-<?php declare(strict_types=1);
+<?php declare (strict_types = 1);
 
 function verificaSeEmailEhValido(): string
 {
-    return filter_var($_GET['email'], FILTER_VALIDATE_EMAIL) ? "O seu e-mail é: " . $_GET['email'] : "O seu e-mail não é válido";
+    $mail = $_GET['email'];
+    $successMsg = "O seu e-mail é: " . $mail;
+    $errMsg = "O seu e-mail não é válido";
+
+    if (filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+        return $successMsg;
+    } else {
+        return $errMsg;
+    }   
 }
 
 echo verificaSeEmailEhValido();
